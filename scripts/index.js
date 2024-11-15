f8a42862d1fd155ebecd942d80e89fdf82fe4273
Rafael Mandel <rafa80744@gmail.com>
2024-11-15 18:22:28 -0300

    feat - adicao do login como popup na index

---
+++ scripts/index.js
@@ -0,0 +1,35 @@
+//tentativa de um modo dark
+const themeToggle = document.getElementById("theme-toggle");
+
+themeToggle.addEventListener("change", () => {
+    document.body.classList.toggle("dark-mode", themeToggle.checked);
+});
+
+//redirecionar as paradas
+function navigateTo(section){
+    window.location.hash = section;
+}
+
+//abrir o login e depois fechar
+function openLoginModal(){
+    document.getElementById("login-modal").style.display = "block";
+}
+
+function closeLoginModal(){
+    document.getElementById("login-modal").style.display = "none";
+}
+
+//a partir daqui e so pra tras :DDD
+function handleLogin(event){
+    event.preventDefault();
+
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    if (username === "admin" && password === "admin"){
+        alert("Login efetuado com sucesso");
+        window.location.href = "home.html"
+    } else{
+        alert("Usuario ou senha incorretos");
+    }
+}
